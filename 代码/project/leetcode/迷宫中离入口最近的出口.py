8bedbf3a6b5abecbf832c318d7a8aed654b0cb0a
wyx <515250339@qq.com>
2022-07-20 08:35:13 +0800

    feat: 新增快排，理解迷宫算法

--- 代码/project/leetcode/迷宫中离入口最近的出口.py
+++ 代码/project/leetcode/迷宫中离入口最近的出口.py
@@ -1,35 +1,50 @@
+# 1926 https://leetcode.cn/problems/nearest-exit-from-entrance-in-maze/
+
+
 # 方法一：广度优先搜索
 from collections import deque
 from typing import List
 
 
-def nearestExit(maze: List[List[str]], entrance: List[int]) -> int:
-    m, n = len(maze), len(maze[0])
-    # 上下左右四个相邻坐标对应的行列变化量
-    dx = [1, 0, -1, 0]
-    dy = [0, 1, 0, -1]
-    # 入口加入队列并修改为墙
-    q = deque([(entrance[0], entrance[1], 0)])
-    maze[entrance[0]][entrance[1]] = '+'
-    while q:
-        cx, cy, d = q.popleft()
-        # 遍历四个方向相邻坐标
-        for k in range(4):
-            nx = cx + dx[k]
-            ny = cy + dy[k]
-            if 0 <= nx < m and 0 <= ny < n and maze[nx][ny] == '.':
-                # 新坐标合法且不为墙
-                if nx == 0 or nx == m - 1 or ny == 0 or ny == n - 1:
-                    # 新坐标为出口，返回距离作为答案
-                    return d + 1
-                # 新坐标为空格子且不为出口，修改为墙并加入队列
-                maze[nx][ny] = '+'
-                q.append((nx, ny, d + 1))
-    # 不存在到出口的路径，返回 -1
-    return -1
+class Solution(object):
+    def nearestExit(self, maze, entrance):
+        """
+        :type maze: List[List[str]]
+        :type entrance: List[int]
+        :rtype: int
+        """
+        # 高宽
+        m, n = len(maze), len(maze[0])
+        # 给定可移动范围 左右 +-1 上下 +-1
+        dx = [1, 0, -1, 0]
+        dy = [0, 1, 0, -1]
+        # 获取当前位置 写入队列, 0为移动步长
+        q = deque([(entrance[0], entrance[1], 0)])
+        # 将当前位置也看作墙
+        maze[entrance[0]][entrance[1]] = "+"
+        # 开始寻找出口
+        while q:
+            # 获取当前位置以及步长
+            cx, cy, d = q.popleft()
+            # 开始移动 4为上下左右
+            for k in range(4):
+                # 左右移动
+                nx = cx + dx[k]
+                # 上下移动
+                ny = cy + dy[k]
+                # 当前坐标合法，也就是大于0且未超出边界并且在输入的二维数组找到
+                if 0 <= nx < m and 0 <= ny < n and maze[nx][ny] == '.':
+                    # 新坐标是否为出口，返回距离
+                    if nx == 0 or nx == m - 1 or ny == 0 or ny == n - 1:
+                        return d + 1
+                    # 将走过的地方定义为墙
+                    maze[nx][ny] = "+"
+                    # 继续移动，寻找出口
+                    q.append((nx, ny, d + 1))
+        # 不存在出口
+        return -1
 
 
 maze = [["+", "+", "+"], [".", ".", "."], ["+", "+", "+"]]
 entrance = [1, 0]
-print(nearestExit(maze, entrance)
-      )
+print(Solution().nearestExit(maze, entrance))
